60a19f6463e6ed3fd6372067fe5f1db28e6393a3
rohanshinde8725 <rohanshinde8725@gmail.com>
2026-07-14 21:21:02 +0530

    App Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes";
+import AppRoutes from "./routes/Index";
 import ScrollToTop from "./components/scrollToTop/ScrollToTop";
 
 function App() {
