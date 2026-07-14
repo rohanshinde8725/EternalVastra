37f8c4d1cd7084afe99dbcd21dd89ab981fe176e
rohanshinde8725 <rohanshinde8725@gmail.com>
2026-07-14 21:35:29 +0530

    Font Fix

--- src/components/categoryBanner/CategoryBanner.jsx
+++ src/components/categoryBanner/CategoryBanner.jsx
@@ -16,23 +16,24 @@ const CategoryBanner = () => {
                 {/* Overlay Content */}
                 <div className="relative z-10 w-full lg:w-1/2 h-full flex flex-col justify-center px-6 sm:px-8 md:px-10 lg:px-12 gap-2 sm:gap-3">
                     <FadeUp delay={0.2}>
-                        <h3 className="text-[#74202D] uppercase text-[10px] sm:text-xs md:text-xl font-bold">
+                        <h3 className="text-[#74202D] uppercase text-[10px] sm:text-xs md:text-sm font-bold">
                             New Arrivals
                         </h3>
                     </FadeUp>
                     <FadeUp delay={0.4}>
-                        <h1 className="text-lg sm:text-xl md:text-2xl lg:text-4xl text-[#440710] font-semibold">
+                        <h1 className="text-lg sm:text-xl md:text-3xl text-[#440710] font-semibold">
                             Fresh Waves <br /> For Every You
                         </h1>
                     </FadeUp>
                     <FadeUp delay={0.6}>
-                        <p className="text-xs sm:text-sm md:text-xl">
+                        <p className="text-xs sm:text-sm md:text-base">
                             Explore the latest sarees <br /> in trend.
                         </p>
                     </FadeUp>
                     <FadeUp delay={0.8}>
                         <button className="bg-[#74202D] text-white uppercase py-1 px-3 sm:px-4 md:px-5 rounded-sm cursor-pointer
-                            hover:bg-white border-2 border-[#74202D] hover:text-[#74202D] transition text-[10px] sm:text-xs md:text-lg font-semibold mt-2 w-fit">
+                            hover:bg-white border-2 border-[#74202D] hover:text-[#74202D] transition text-[10px] sm:text-xs 
+                            md:text-sm font-semibold mt-2 w-fit">
                             Explore Now
                         </button>
                     </FadeUp>
@@ -51,24 +52,24 @@ const CategoryBanner = () => {
                 {/* Content */}
                 <div className="relative z-10 w-full lg:w-1/2 h-full flex flex-col justify-center px-6 sm:px-8 md:px-10 lg:px-12 gap-2 sm:gap-3">
                     <FadeUp delay={0.2}>
-                        <h3 className="text-[#74202D] uppercase text-[10px] sm:text-xs md:text-xl font-bold">
+                        <h3 className="text-[#74202D] uppercase text-[10px] sm:text-xs md:text-base font-bold">
                             Festive Offer
                         </h3>
                     </FadeUp>
                     <FadeUp delay={0.4}>
-                        <h1 className="text-lg sm:text-xl md:text-2xl lg:text-4xl text-[#440710] font-semibold">
+                        <h1 className="text-lg sm:text-xl md:text-3xl text-[#440710] font-semibold">
                             Up To 30% Off
                         </h1>
                     </FadeUp>
                     <FadeUp delay={0.6}>
-                        <p className="text-xs sm:text-sm md:text-xl">
+                        <p className="text-xs sm:text-sm md:text-base">
                             On Selected <br /> Saree Collections.
                         </p>
                     </FadeUp>
                     <FadeUp delay={0.8}>
                         <button className="bg-[#74202D] text-white uppercase py-1 px-3 sm:px-4 md:px-5 rounded-sm cursor-pointer
                             hover:bg-white border-2 border-[#74202D] hover:text-[#74202D] transition
-                            text-[10px] sm:text-xs md:text-lg font-semibold mt-2 w-fit">
+                            text-[10px] sm:text-xs md:text-sm font-semibold mt-2 w-fit">
                             Shop Sale
                         </button>
                     </FadeUp>
